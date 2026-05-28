db6172ee11f4ae7e05b4b30540414cf1b71f19d1
Janhavi K <janhavik626@gmail.com>
2026-05-28 12:21:07 +0530

    Update category.py

--- category.py
+++ category.py
@@ -15,7 +15,11 @@ def read_excel(file):
     df = pd.read_excel(file)
     return df
 
-df = pd.read_excel('data\sales.xls')
+import os
+import pandas as pd
+
+file_path = os.path.join(os.path.dirname(__file__), "data", "sales.xls")
+df = pd.read_excel(file_path)
 
 # -----------------------------
 # Page Title
